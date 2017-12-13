aca4c22e2fb6b5f0ace4205b90173ef6b05fc135
Eduardo Rascon <eduardo.rascon@gmail.com>
2017-12-12 20:53:12 -0600

    Splash theme added to the SplashActivity.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/SplashActivity.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.eduardorascon.pocsplashscreen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
     }
 }
