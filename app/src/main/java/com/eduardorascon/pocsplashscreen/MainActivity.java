1863bc87f4292cc076debac3388eb5c721f91bd4
eduardorascon <eduardo.rascon@gmail.com>
2017-12-30 20:49:54 -0600

    Bottom navigation item selected added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/MainActivity.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/MainActivity.java
@@ -1,7 +1,10 @@
 package com.eduardorascon.pocsplashscreen;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                            case R.id.action_prueba:
+                            case R.id.action_prueba2:
+                        }
+
+                        return true;
+                    }
+                }
+        );
+
     }
 }
