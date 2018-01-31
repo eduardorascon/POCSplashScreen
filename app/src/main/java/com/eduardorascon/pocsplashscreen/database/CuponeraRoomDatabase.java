89d226714ad5039558719d0b319b2929ce42d594
Eduardo Rascon <eduardo.rascon@gmail.com>
2018-01-30 21:37:08 -0600

    Room database implemented.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/CuponeraRoomDatabase.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/CuponeraRoomDatabase.java
@@ -1,4 +1,35 @@
 package com.eduardorascon.pocsplashscreen.database;
 
-public abstract class CuponeraRoomDatabase {
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+public abstract class CuponeraRoomDatabase extends RoomDatabase {
+    private static CuponeraRoomDatabase INSTANCE;
+
+    public static CuponeraRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE != null)
+            return INSTANCE;
+
+        synchronized (CuponeraRoomDatabase.class) {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        CuponeraRoomDatabase.class, "cuponera_database")
+                        .addCallback(sRoomDatabaseCallback)
+                        .build();
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+        }
+    };
 }
