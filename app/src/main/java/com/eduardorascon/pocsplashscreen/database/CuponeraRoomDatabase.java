77959d5d46606984532b088cec8ca5cc9942c7a6
Eduardo Rascon <eduardo.rascon@gmail.com>
2018-01-30 21:49:14 -0600

    Product entity created.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/CuponeraRoomDatabase.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/CuponeraRoomDatabase.java
@@ -1,11 +1,13 @@
 package com.eduardorascon.pocsplashscreen.database;
 
 import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
+@Database(entities = {Product.class}, version = 1)
 public abstract class CuponeraRoomDatabase extends RoomDatabase {
     private static CuponeraRoomDatabase INSTANCE;
 
--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/Product.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/Product.java
@@ -1,8 +1,23 @@
 package com.eduardorascon.pocsplashscreen.database;
 
-/**
- * Created by lalo-cota on 1/30/18.
- */
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
+@Entity(tableName = "products")
 public class Product {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "product_name")
+    private String mProduct;
+
+    public Product(String product) {
+        this.mProduct = product;
+    }
+
+    public String getProduct() {
+        return mProduct;
+    }
 }
