f2fffcf6e5df5f66f48b9b751cc1e49c31904f72
Eduardo Rascon <eduardo.rascon@gmail.com>
2018-02-01 21:54:08 -0600

    ProductsDao has methods.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/ProductDao.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/ProductDao.java
@@ -1,8 +1,20 @@
 package com.eduardorascon.pocsplashscreen.database;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
+import java.util.List;
 
 @Dao
 public interface ProductDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Product product);
+
+    @Query("DELETE FROM products")
+    void deleteAll();
+
+    @Query("SELECT * FROM products ORDER BY word ASC")
+    List<Product> getAllProducts();
 }
