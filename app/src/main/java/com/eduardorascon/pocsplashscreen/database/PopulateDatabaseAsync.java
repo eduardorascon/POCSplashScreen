d205c4ba938a2c7092a64d2ab90c3f0030c7c2d3
eduardorascon <eduardo.rascon@gmail.com>
2018-02-03 23:10:52 -0600

    PopulateDatabaseAsync implemented.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/PopulateDatabaseAsync.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/PopulateDatabaseAsync.java
@@ -1,8 +1,16 @@
 package com.eduardorascon.pocsplashscreen.database;
 
-/**
- * Created by User on 02/02/2018.
- */
+import android.os.AsyncTask;
 
-public class PopulateDatabaseAsync {
+public class PopulateDatabaseAsync extends AsyncTask<Void, Void, Void> {
+    private final ProductDao mProductsDao;
+
+    public PopulateDatabaseAsync(CuponeraRoomDatabase db) {
+        mProductsDao = db.productDao();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
 }
