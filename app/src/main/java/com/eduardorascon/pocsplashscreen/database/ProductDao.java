8c35cc64abe7eefa527cd7615befa557dc386f3c
eduardorascon <eduardo.rascon@gmail.com>
2018-02-04 22:27:23 -0600

    Insert and getAllProducts added to ProductDao interface.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/ProductDao.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/ProductDao.java
@@ -1,8 +1,17 @@
 package com.eduardorascon.pocsplashscreen.database;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
+import java.util.List;
 
 @Dao
 public interface ProductDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Product product);
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    List<Product> getAllProducts();
 }
