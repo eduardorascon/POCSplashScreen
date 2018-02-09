7ff3fcc3775dc6137bacb2266d3807759fe9e0b8
eduardorascon <eduardo.rascon@gmail.com>
2018-02-08 22:20:06 -0600

    Column id added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/Category.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/Category.java
@@ -1,4 +1,20 @@
 package com.eduardorascon.pocsplashscreen.database;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "categories")
 public class Category {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "category_id")
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
