be2b294c153b0e3edfab485116612672cfcbf048
Eduardo Rascon <eduardo.rascon@gmail.com>
2018-02-10 21:38:05 -0600

    CategoryDao interface implemented. Insert, deleteAll and getAllCategories added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/CategoryDao.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/CategoryDao.java
@@ -1,8 +1,21 @@
 package com.eduardorascon.pocsplashscreen.database;
 
-/**
- * Created by User on 09/02/2018.
- */
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 
+import java.util.List;
+
+@Dao
 public interface CategoryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Category category);
+
+    @Query("DELETE from categories")
+    void deleteAll();
+
+    @Query("SELECT * FROM categories ORDER BY category_name ASC")
+    List<Category> getAllCategories();
 }
