0b17b0a5ee56ff1818d5290dcf777ad841c10cf8
eduardorascon <eduardo.rascon@gmail.com>
2018-02-16 18:31:19 -0600

    CategoryAllProducts private fields added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/CategoryAllProducts.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/CategoryAllProducts.java
@@ -1,2 +1,7 @@
 public class CategoryAllProducts {
+	@Embedded
+	private Category category;
+
+	@Relation(parentColumn = "category_id", entityColumn = "category_id", entity = Product.class)
+	private List<Product> products;
 }
