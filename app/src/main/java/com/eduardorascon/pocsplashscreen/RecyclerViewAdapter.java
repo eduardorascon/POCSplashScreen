de7fb643af28cae1e6b92f91e91329d5bb99db90
eduardorascon <eduardo.rascon@gmail.com>
2018-02-23 17:27:40 -0600

    Constructor and onCreateViewHolder functions added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/RecyclerViewAdapter.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/RecyclerViewAdapter.java
@@ -1,3 +1,12 @@
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
-	
+
+	public RecyclerViewAdapter(List<Category> categoryList) {
+
+	}
+
+	@Override
+	public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		return new RecyclerViewHolder(LayoutInflater.from(parent.getContext()))
+			.inflate(R.layout.recycler_item, parent, false);
+	}
 }
