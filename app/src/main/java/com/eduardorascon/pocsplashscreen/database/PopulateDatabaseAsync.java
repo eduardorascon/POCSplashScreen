e3b0397238ff89cc5e213a095973a64c8e85ae34
Eduardo Rascon <eduardo.rascon@gmail.com>
2018-02-25 20:22:47 -0600

    CategoryDao implemented into PopulateDatabaseAsync

--- app/src/main/java/com/eduardorascon/pocsplashscreen/database/PopulateDatabaseAsync.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/database/PopulateDatabaseAsync.java
@@ -4,9 +4,11 @@ import android.os.AsyncTask;
 
 public class PopulateDatabaseAsync extends AsyncTask<Void, Void, Void> {
     private final ProductDao mProductsDao;
+    private final CategoryDao mCategoryDao;
 
     public PopulateDatabaseAsync(CuponeraRoomDatabase db) {
         mProductsDao = db.productDao();
+        mCategoryDao = db.categoryDao();
     }
 
     @Override
