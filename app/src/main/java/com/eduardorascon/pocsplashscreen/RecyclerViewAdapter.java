2daef7938434cfd24ecc9850347ff871fa534d62
eduardorascon <eduardo.rascon@gmail.com>
2018-02-26 18:56:40 -0600

    Function getItemCount added.

--- app/src/main/java/com/eduardorascon/pocsplashscreen/RecyclerViewAdapter.java
+++ app/src/main/java/com/eduardorascon/pocsplashscreen/RecyclerViewAdapter.java
@@ -1,12 +1,19 @@
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
 
-	public RecyclerViewAdapter(List<Category> categoryList) {
+	private List<Category> categoryList;
 
+	public RecyclerViewAdapter(List<Category> categoryList) {
+		this.categoryList = categoryList;
 	}
 
 	@Override
 	public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		return new RecyclerViewHolder(LayoutInflater.from(parent.getContext()))
 			.inflate(R.layout.recycler_item, parent, false);
+	}
+
+	@Override
+	public int getItemCount() {
+		return 0;
 	}
 }
